8efbd229662139637d675d476ee29f2473e232ef
Andrei Boyarko <andreyboyarko@gmail.com>
2026-06-26 12:18:15 +0200

    Initial commit

---
+++ NoblePatternSymptomAtlas/NoblePatternSymptomAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoblePatternSymptomAtlasApp.swift
+//  NoblePatternSymptomAtlas
+//
+//  Created by Andrei  Boyarko on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoblePatternSymptomAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
